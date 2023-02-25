7c3d805b3ba0915f1ff73872fe0302152e4a3a7e
blckRbbit <lex.leonidov@gmail.com>
2023-02-25 16:13:17 +0600

    added homework

---
+++ src/homework/Main.java
@@ -0,0 +1,9 @@
+package homework;
+
+public class Main{
+    public static void main(String[] args){
+        User user = new User("Bob");
+        user.report();
+        user.save();
+    }
+}
---
+++ src/homework/Persister.java
@@ -0,0 +1,13 @@
+package homework;
+
+public class Persister{
+    private final User user;
+
+    public Persister(User user){
+        this.user = user;
+    }
+
+    public void save(){
+        System.out.println("Save user: " + user.getName());
+    }
+}
