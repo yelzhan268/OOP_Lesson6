30f0c1c5b1fb355d79aa51776ccea85f5cee5725
shary2023 <128229183+shary2023@users.noreply.github.com>
2023-07-14 17:36:41 +0600

    encoding changed

--- src/dip1/model/Report.java
+++ src/dip1/model/Report.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Report{
     private List<ReportItem> items;	// report data
 
-    // расчет отчетных данных
+    // calculate report data
     public void calculate(){
         items =  new ArrayList<ReportItem>();
         items.add(new ReportItem("First", (float)5));
